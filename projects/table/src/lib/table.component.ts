5fa918c45ece124bd2cebf39a7bac2ccf434124f
Phạm Đức Thông <thong19981110vn@MacBook-Air-cua-Pham.local>
2022-07-18 17:08:42 +0700

    fisrt commit

---
+++ projects/table/src/lib/table.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-table',
+  template: `
+    <p>
+      table works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
